e564f7b72047d5b63c9531f4e79852f78c0361a0
Алексей Вернер <werneralexey@mail.ru>
2022-08-09 16:02:18 +0500

    Создал структуру проекта

---
+++ si_test/app/routes.py
@@ -0,0 +1,29 @@
+from aiohttp import web
+
+
+
+from app.context import AppContext
+
+
+def wrap_handler(handler, context):
+    async def wrapper(request):
+        return await handler(request, context)
+
+    return wrapper
+
+
+def setup_routes(app: web.Application, ctx: AppContext) -> None:
+    app.router.add_get(
+        '/v1/',
+        wrap_handler(
+            'My handle', # TODO
+            ctx,
+        ),
+    )
+    app.router.add_get(
+        '/v1/',
+        wrap_handler(
+            'My handle', # TODO
+            ctx,
+        ),
+    )
