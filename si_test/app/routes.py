738576e4f7c1ddb9d455f4bd2e63db9fd2f3e645
Алексей Вернер <werneralexey@mail.ru>
2022-08-15 13:22:27 +0500

    update routes.py

--- si_test/app/routes.py
+++ si_test/app/routes.py
@@ -13,49 +13,49 @@ def wrap_handler(handler, context):
 
 def setup_routes(app: web.Application, ctx: AppContext) -> None:
     app.router.add_post(
-        '/v1/auth/signup',
+        '/v1/auth/signup/',
         wrap_handler(
             auth.signup,
             ctx
         )
     )
     app.router.add_post(
-        '/v1/auth/login',
+        '/v1/auth/login/',
         wrap_handler(
             auth.login,
             ctx
         )
     )
     app.router.add_get(
-        '/v1/users',
+        '/v1/users/',
         wrap_handler(
             users.get_users_list,
             ctx
         )
     )
     app.router.add_get(
-        r'/v1/users/{user_id:\d+}',
+        r'/v1/users/{user_id:\d+}/',
         wrap_handler(
             users.get_users_detail,
             ctx
         )
     )
     app.router.add_put(
-        r'/v1/users/{user_id:\d+}',
+        r'/v1/users/{user_id:\d+}/',
         wrap_handler(
             users.update_patch_user,
             ctx
         )
     )
     app.router.add_patch(
-        r'/v1/users/{user_id:\d+}',
+        r'/v1/users/{user_id:\d+}/',
         wrap_handler(
             users.update_patch_user,
             ctx
         )
     )
     app.router.add_delete(
-        r'/v1/users/{user_id:\d+}',
+        r'/v1/users/{user_id:\d+}/',
         wrap_handler(
             users.delete_user,
             ctx
